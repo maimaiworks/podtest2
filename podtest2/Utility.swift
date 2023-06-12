4bde1957b08eb85c38d7648b38bd5525d6c41660
HIroshi Imai <im@i-hiroshi.com>
2023-06-12 22:42:56 +0900

    project

---
+++ podtest2/Utility.swift
@@ -0,0 +1,16 @@
+//
+//  Utility.swift
+//  podtest1
+//
+//  Created by Imai Hiroshi on 5/11/23.
+//
+
+import UIKit
+
+public final class Utility: NSObject {
+	
+	public func sayHello() -> String {
+		return "hogehoge Hello!!"
+	}
+
+}
---
+++ podtest2/podtest2.h
@@ -0,0 +1,18 @@
+//
+//  podtest2.h
+//  podtest2
+//
+//  Created by Imai Hiroshi on 6/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for podtest2.
+FOUNDATION_EXPORT double podtest2VersionNumber;
+
+//! Project version string for podtest2.
+FOUNDATION_EXPORT const unsigned char podtest2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <podtest2/PublicHeader.h>
+
+
